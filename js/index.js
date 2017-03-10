7e1df279b3f0b4bd9aa2020c9758b68c8d0c6700
Francisco Alvariño <falvarino@sugit.com.ar>
2017-03-10 16:39:51 -0300

    no message

--- js/index.js
+++ js/index.js
@@ -42,17 +42,16 @@ var app = {
 		mostrarMensaje("so: "+devicePlatform.toUpperCase());
 		if(devicePlatform.toUpperCase()=="IOS") {
 			
-			audioRecord = 'documents://record.wav';
+			audioRecord = 'record.wav';
+					
+			myMedia = new Media(audioRecord, onSuccess, onError);
+			myMedia.startRecord();
+			myMedia.stopRecord();
 			
 			window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onSuccess, onError);
 			window.requestFileSystem(LocalFileSystem.TEMPORARY, 0, gotFS, fail);
 			basePath_pg = getPhoneGapPath();
-			//~ basePath_pg = '';
-			myMedia = new Media(audioRecord, onSuccess, onError);
-			myMedia.startRecord();
-			myMedia.stopRecord();
-			//~ myMedia.release();
-			
+			basePath_pg = '';
 		} else {
 			audioRecord = cordova.file.externalRootDirectory+'record.arm';
                 
@@ -96,16 +95,13 @@ function gotFS(fileSystem) {
 
 function gotFileEntry(fileEntry) {
 	fileURL = fileEntry.toURL();
-	//~ audioRecord = entry.toURI();
 	//~ alert(fileURL);
-	
-	mostrarMensaje("este archivo: "+audioRecord);
 }
 
 function mostrarMensaje(msj)
 {
 	var cont = document.getElementById("responde");
-	cont.innerHTML = msj;
+	//~ cont.innerHTML = msj;
 }
 
 var posic = 0;
@@ -124,7 +120,6 @@ function startRecording(duracion)
 	myMedia = new Media(audioRecord, onSuccess, onError);
 	myMedia.startRecord();
 	tiempoTranscurrido = 0;
-	
 	superinterval = setInterval(function() {
 		window.updateVisualizer();
 	},100);
@@ -138,7 +133,6 @@ function startRecording(duracion)
  
 function resetGrabacion() {
 	myMedia.stopRecord();
-	console.log("reset");
 	isRecording = false;
 	mostrarMensaje("Grabacion finalizada");
 	$('#sec6-1-player-equelizer').html('');
@@ -158,9 +152,6 @@ function resetGrabacion() {
 	}
 	duracion = 0;
 	if(isFinlaPlay) {
-		
-		isFinlaPlay = false;
-		$('#btn-step7-play').html('<img src="imgs/ico-play.png">');
 		finalAudio.stop();
 	}
 	clearInterval(superinterval);
